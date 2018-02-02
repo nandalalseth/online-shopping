61a22a18c290ccdda6e21c9745872e912989060d
nandalalseth <nandalalseth@192.168.1.6>
2018-02-02 22:13:25 +0000

    Project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/nandalal/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.nandalal.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
